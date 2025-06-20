e8218a3bcbc7ba9cae49f4d6156dc880954b434b
Shashwat <shashwattripathi121@gmail.com>
2025-06-20 07:19:09 +0530

    Added trading.py

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 This is a web application that uses the alpaca API in the back end to buy and sell shares on the market, in paper trading. i.e. no actual currency is used. The entire back end is made on Python
 
+## Alpaca API
+
+It lets you make trades during its own trading environment. You can set up various trade variables to make trades in the right way.
---
+++ Trading.py
@@ -0,0 +1,85 @@
+import alpaca_trade_api as tradeapi
+
+# Authentication of API using API key and secret
+API_KEY = 'PKEAPFGSLDAKSZZEH9G2'
+API_SECRET = 'KfTAPbTBj7TTcsXHWoOpDhkARsJkCmLj5AIgrqGe'
+BASE_URL = 'https://paper-api.alpaca.markets'
+
+#initialize the api
+api=tradeapi.REST(API_KEY, API_SECRET, BASE_URL, api_version='v2')
+
+# Check account info
+account = api.get_account()
+
+def check_balance():
+    print("Account balance = ",account.cash)
+
+# Place a paper trade order/Buy (1 share of nvidia)
+def Buy_Nvidia_shares():
+    global order
+    order = api.submit_order(
+        symbol="NVDA", # The symbol assigned to nvidia
+        qty=1, # Buying 2 shares of apple
+        side='buy', # Buy Action
+        type='market', # Market Order (Executed immediately)
+        time_in_force='gtc'
+    )
+    print("Buy order submitted for NVDA")
+
+#Check Open Orders
+def check_open_orders():
+    open_orders=api.list_orders(status='open')
+    if open_orders:
+        print("Open Orders:")
+        for order in open_orders:
+            print(f"Order ID: {order.id} - {order.side} {order.qty} of {order.symbol} at {order.created_at}")
+    else:
+        print("No Open Orders")
+
+# Check Current Holdings or Positions
+def check_current_positions():
+    positions=api.list_positions()
+    if positions:
+        print("Current Positions:")
+        for position in positions:
+            print(f"Symbol: {position.symbol} | Quantity: {position.qty} shares")
+
+#Sell a share
+def Sell_share():
+    import time
+    while True:
+        status = api.get_order(order.id).status
+        if status == "filled":
+            print("Order Filled")
+            break
+        time.sleep(1)
+
+    api.submit_order(
+        symbol="NVDA",
+        qty=1,
+        side='sell',
+        type='market',
+        time_in_force='day'
+    )
+    print("Sell order submitted for NVDA")
+
+def close_all_positions():
+    positions = api.list_positions()
+    for position in positions:
+        side = 'sell' if float(position.qty) > 0 else 'buy'
+        api.submit_order(
+            symbol=position.symbol,
+            qty=abs(int(float(position.qty))),
+            side=side,
+            type='market',
+            time_in_force='day'
+        )
+
+def check_status():
+    status = api.get_order(order.id).status
+    print(status)
+
+Buy_Nvidia_shares()
+check_open_orders()
+check_status()
+
